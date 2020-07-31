dc10dc38c938a64a0c497dbeffb3cfea330f0cb2
Milos Popovic <milos_popovic@outlook.com>
2020-07-31 17:17:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,13 @@
-class Song
+class Appointment
 
   @@all = []
 
   attr_accessor :name, :artist, :genre
 
-  def initialize(name, artist, genre)
-    @name = name
-    @artist = artist
-    @genre = genre
+  def initialize(date, doctor, patient)
+    @date = date
+    @doctor = doctor
+    @patient = patient
     @@all << self
   end
 
