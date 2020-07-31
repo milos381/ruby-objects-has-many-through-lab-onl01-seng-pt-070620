20b4edcb5e1c837ca635b005c743239fd420ff9c
Milos Popovic <milos_popovic@outlook.com>
2020-07-31 16:54:04 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,33 @@
+class Artist
+
+  @@all = []
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @@all << name
+  end
+
+  def self.all
+    @@all
+  end
+
+
+  def new_song(name, genre)
+    Song.new(name, genre, self)
+  end
+
+  def songs
+    Song.all.select do |song|
+      song.artist == self
+    end
+  end
+
+  def genres
+    songs.each do |song|
+      song.genre
+    end
+  end
+
+end
